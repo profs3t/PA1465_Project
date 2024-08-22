b591f5847f172515174fcba88cebafb05cc4eb26
profs3t <theodor.maran@gmail.com>
2024-08-22 15:35:28 +0200

    Test

--- compare.py
+++ compare.py
@@ -38,9 +38,9 @@ def compare_files(filenames):
         for i in range(len(filenames[criteria])):
             for j in range(i+1, len(filenames[criteria])):
                 if not compareHashes(filenames[criteria][i], filenames[criteria][j]):
-                    print(f"Files {filenames[criteria][i]} and {filenames[criteria][j]} are different")
-                else:
-                    print(f"Files {filenames[criteria][i]} and {filenames[criteria][j]} are the same")
+                    # Write to a file
+                    with open("different_files.txt", "a") as f:
+                        f.write(f"Files {filenames[criteria][i]} and {filenames[criteria][j]} are different\n")
 
 def main():
     os_dict, version_dict = read_filenames()
