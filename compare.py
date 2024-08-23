8dd23965e02c497071bc5034313c3ae37cc88d5a
profs3t <theodor.maran@gmail.com>
2024-08-23 16:26:19 +0200

    Test

--- compare.py
+++ compare.py
@@ -3,6 +3,7 @@
 import hashlib
 from pathlib import Path
 import pickle
+from generate_data import test_dict
 
 WRITE_FILE = open('example.txt', 'a+', encoding='utf-8')
 
@@ -27,16 +28,6 @@ def compare_hashes(filename1, filename2):
     if hash1 != hash2:
         WRITE_FILE.write(f"Files {filename1} and {filename2} are different\n")
     return hash1 == hash2
-
-def compare_objects(filename1, filename2):
-    """ Compare the hashes of two files """
-    obj1 = get_object(filename1)
-    obj2 = get_object(filename2)
-    # Compare the objects
-    if obj1 != obj2:
-        WRITE_FILE.write(f"Objects {filename1} and {filename2} are different\n")
-    return obj1 == obj2
-
 
 def generate_filenames():
     """ Generate filenames for the different operating systems and python versions """
@@ -77,27 +68,22 @@ def compare_files(filenames):
 
 def compare_deserialized_data(filenames):
     """ Compare the files """
+    # Compare each file with the original object
     success_count = 0
     fail_count = 0
-    # Compare files within the same operating system
-    for os_files in filenames:
-        for i in range(len(os_files)):
-            for j in range(i + 1, len(os_files)):
-                result = compare_objects(os_files[i], os_files[j])
-                if result:
-                    success_count+=1
-                else:
-                    fail_count+=1
-
-    # Compare files across different operating systems for the same python version
-    for version_index in range(len(filenames[0])):
-        for i in range(len(filenames)):
-            for j in range(i + 1, len(filenames)):
-                resualt = compare_objects(filenames[i][version_index], filenames[j][version_index])
-                if resualt:
-                    success_count+=1
-                else:
-                    fail_count+=1
+    
+    for operating_system in filenames:
+        for version in operating_system:
+            data = get_object(version)
+            original_set = set(test_dict.items())
+            deserialized_set = set(data.items())
+            difference = original_set ^ deserialized_set
+            #If lenght of difference > 0
+            if difference:
+                fail_count+=1
+                WRITE_FILE.write(f"Data in file {version} is different from the original data\n")
+            else:
+                success_count+=1
 
     print("Success:", success_count)
     print("Fail:", fail_count)
